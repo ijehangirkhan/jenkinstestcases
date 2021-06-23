c960f86fb1527e1ac140b269a423df8bb478245e
Jehangir Khan <jehangirkhan@Apples-iMac-2.local>
2021-06-24 01:31:01 +0500

    Test change 3.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(calculator.add(10, 5), 15)
-        self.assertEqual(calculator.add(-1, 1), 2)
+        self.assertEqual(calculator.add(-1, 1), 0)
         self.assertEqual(calculator.add(-1, -1), -2)
 
     def test_subtract(self):
@@ -16,7 +16,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(calculator.subtract(-1, -1), 0)
 
     def test_multiply(self):
-        self.assertEqual(calculator.multiply(10, 5), 50)
+        self.assertEqual(calculator.multiply(10, 5), 5)
         self.assertEqual(calculator.multiply(-1, 1), -1)
         self.assertEqual(calculator.multiply(-1, -1), 1)
 
